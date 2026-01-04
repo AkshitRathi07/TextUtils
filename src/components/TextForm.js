8ed8b88b9ecfbaf3830749d76bb9d4b5e4b7317c
AkshitRathi07 <aksethrathi@gmail.com>
2026-01-04 07:18:53 +0530

    Final TextUtils Code

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -61,7 +61,7 @@ export default function TexthtmlForm(props) {
   </div>
   <div className="container my-3" style={{color:props.mode==='dark'?'white':'#042743'}}>        
      <h2>This is your text summary</h2>
-     <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+     <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
      <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} minutes taken to read it</p>
      <h2>Preview</h2>
      <p>{text.length>0 ? text:"Nothing to Preview!"}</p>
